1bde04e245899b602879862f73316fc3b548e08f
Kwlad1ck <vlad-kad00@mail.ru>
2024-03-03 13:46:14 +0300

    delete sleep

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -106,7 +106,6 @@ public class MainPage {
     @Step("Нажатие кнопки подтверждения")
     public MainPage clickSignBtn() {
         signBtn.click();
-        sleep(15000);
 
         return this;
     }
